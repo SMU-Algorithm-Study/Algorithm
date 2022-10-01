93f6d448c97dc468fe9721f49aff9cafe109448b
Lee Hye-In <59090549+hyeinlee725@users.noreply.github.com>
2022-10-01 23:07:43 +0900

    Delete week52 directory

--- week52/카펫_hyeinlee725.py
+++
@@ -1,7 +0,0 @@
-def solution(brown, yellow):
-    carpet = brown + yellow
-    for i in range(carpet, 2, -1):
-        if carpet  % i == 0: 
-            a = carpet // i
-            if yellow == (i - 2) * (a - 2):
-                    return [i, a]
--- week52/피보나치 수_hyeinlee725.py
+++
@@ -1,9 +0,0 @@
-def solution(n):
-    answer = []
-    for i in range(n + 1):
-        if i == 0 or i == 1:
-            answer.append(i)
-        else:
-            fibo = answer[i - 1] + answer[i - 2]
-            answer.append(fibo % 1234567)
-    return answer[-1]
